98bcc8655a607dcbc82d1e2d76e240eafc0a0a3e
viitorfernandess <vitorazevedofer007@gmail.com>
2026-01-22 14:39:29 -0300

    feat: cria método para listar todos os livros

---
+++ src/models/books-model.js
@@ -0,0 +1,11 @@
+const uuid = require('uuid').v4
+
+const books = [ 
+    { id: '1', title: 'Book One', author: 'Author One', quantityAvailable: 4 },
+    { id: '2', title: 'Book Two', author: 'Author Two', quantityAvailable: 3 }
+]
+
+
+module.exports = {
+    getAllBooks: () => books,
+}
