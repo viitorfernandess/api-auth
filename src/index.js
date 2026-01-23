386cbcdba0ca8cb0bd2f72443bfb6bf8f1014b4d
viitorfernandess <vitorazevedofer007@gmail.com>
2026-01-23 14:51:43 -0300

    fix: corrige roteamento da rota GET /api/books

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 
 app.use('/auth', authRouter)
-app.use('api', apiRouter)
+app.use('/api', apiRouter)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()  => console.log(`Servidor iniciado em http://localhost:${PORT}`) )
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const authController = require('../controllers/auth-controller')
 const authRouter = express.Router()
 
-router.post('/register', authController.register)
-router.post('/login', authController.login)
+authRouter.post('/register', authController.register)
+authRouter.post('/login', authController.login)
 
-module.exports = router
+module.exports = authRouter
