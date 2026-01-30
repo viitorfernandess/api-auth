39fab92992ae95a0cce5f29a059031848dbf1b47
viitorfernandess <vitorazevedofer007@gmail.com>
2026-01-30 14:58:59 -0300

    feat: criar model de empréstimos e listar livros disponíveis

---
+++ src/models/loans-model.js
@@ -0,0 +1,19 @@
+const uuid = require('uuid').v4
+
+const loans = [
+    {
+        id: '1',
+        userId: '1',
+        bookId:  '1',
+        loanDate: new Date('2026-01-01'),
+        returnDate: null,
+        isReturned: false,
+        isLate: true
+    }
+]
+
+module.exports = {
+    getAllLoans: () => loans,
+
+    
+}
